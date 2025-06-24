b86a15c305f7dc5f4eb4d2d46c099c953d84edaf
jordanmontt <sebastianjmt@hotmail.com>
2025-06-24 16:27:15 +0200

    New image command: removing the second name parameter

--- src/image_manager.sh
+++ src/image_manager.sh
@@ -166,18 +166,14 @@ install_image() {
         pharo_version=$DEFAULT_PHARO_VERSION
     fi
 
-    # Image name
-    image_name=${2}
+    #Getting image name
+    current_date=$(date +"%d-%m-%Y-%Hh%M")
+    echo "Enter an image name: (skip to use ${current_date})"
+    read image_name
     if [ -z $image_name ]; then
-        #Getting image name
-        current_date=$(date +"%d-%m-%Y-%Hh%M")
-        echo "Enter an image name: (skip to use ${current_date})"
-        read image_name
-        if [ -z $image_name ]; then
-            image_name=$current_date
-        fi
+        image_name=$current_date
     fi
-
+    
     # Creating folder and go
     _go_to_pharo_images_folder_
     mkdir $image_name
